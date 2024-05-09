b656b577e14f8d19ccb53d8ce00dffc7383aba18
moofazzel <mofazzel444@gmail.com>
2024-05-09 22:57:57 +0600

    Handled pending while voting

--- app/components/TrumpVoteButton.js
+++ app/components/TrumpVoteButton.js
@@ -1,15 +1,21 @@
-`use client`;
+"use client";
+
+import { useFormStatus } from "react-dom";
 
 const TrumpVoteButton = () => {
+  const { pending } = useFormStatus();
+
   return (
     <div className="flex justify-center mt-5">
       <button
-        type="submit"
-        className="flex   w-full md:w-[300px] sm:w-[200px]  justify-center gap-2 items-center bg-blue-500 px-6 py-2.5 text-white font-semibold hover:bg-blue-600 group rounded-md tracking-wider  text-base sm:text-xl"
+        disabled={pending}
+        className="flex gap-2 w-full md:w-[300px] sm:w-[200px] justify-center items-center bg-blue-500 px-6 py-2.5 text-white font-semibold hover:bg-blue-600 group rounded-md tracking-wider text-base sm:text-xl disabled:bg-slate-400"
       >
-        <span>Vote</span>
+        <span>{pending ? "Voting..." : "Vote"}</span>
         <svg
-          className="group-hover:-translate-y-[2px] transition-transform duration-300"
+          className={`group-hover:-translate-y-[2px] transition-transform duration-300 ${
+            pending && "animate-pulse"
+          } `}
           stroke="currentColor"
           fill="currentColor"
           stroke-width="0"
